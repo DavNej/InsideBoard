4d6037382c3296a6f18b062c03a677cdd6b1e884
DavNej <davnej.dev@gmail.com>
2019-02-06 09:24:31 +0100

    rendu_de_monnaie is working || Added __str__ representation in <Change class> || Added ask_amount_to_change method || Added get_the_change method || Implemented optimal_change function that returns a instance of the <Change class>

--- monnaie/rendu_de_monnaie.py
+++ monnaie/rendu_de_monnaie.py
@@ -14,37 +14,86 @@ logger = logging.getLogger()
 logger.info('Program started')
 
 class Change():
-  def __init__(self):
-    self.coin2 = None
-    self.bill5 = None
-    self.bill10 = None
-
-    try:
-      self.TOTAL_AMOUNT = int(argv[1])
+  def __init__(self, coin2=0, bill5=0, bill10=0):
+      self.coin2 = coin2
+      self.bill5 = bill5
+      self.bill10 = bill10
     
+
+  def __str__(self):
+    return """
+    {0} pièces de 2 euros
+    {1} billet(s) de 5 euros
+    {2} billet(s) de 10 euros
+    """.format(self.coin2, self.bill5, self.bill10)
+
+
+  def ask_amount_to_change(self, amount=None):
+    try:
+      self.TOTAL_AMOUNT = abs(int(argv[1]))
     except IndexError as err:
       logger.warning(err)
-      logger.warning('No amount was entered')
-      self.getAnInt()
-
+      logger.warning('No arg was entered at program launch - {}'.format(err))
+      self.get_an_int()
     except ValueError as err:
-      logger.warning(f'Amount entered was not an int - {err}')
-      self.getAnInt()
+      logger.warning('Amount entered was not an int - {}'.format(err))
+      self.get_an_int()
+    self.spare = self.TOTAL_AMOUNT
 
-  def getAnInt(self):
+
+  def get_an_int(self):
     self.TOTAL_AMOUNT = None
     while not isinstance(self.TOTAL_AMOUNT, int):
-      print('Veuillez entrer une somme (en chiffre)')
+      print('Veuillez entrer une somme (en chiffre entier)')
       try:
-        self.TOTAL_AMOUNT = int(input())
+        self.TOTAL_AMOUNT = abs(int(input()))
       except ValueError as err:
-        logger.warning(f'Amount entered was not an int - {err}')
-        self.getAnInt()
+        logger.warning('Amount entered was not an int - {}'.format(err))
+        self.get_an_int()
+  
 
+  def get_the_change(self):
+    if (self.spare == 0):
+      # opt_chg = Change(coin2=self.coin2, bill5=self.bill5, bill10=self.bill10)
+      # print(opt_chg)
+      return
+    
+    elif (self.spare == 1 and self.TOTAL_AMOUNT > 1):
+      print('Désolé mais la machine ne peut rendre l\'appoint, vous perdez 1€')
+      # opt_chg = Change(coin2=self.coin2 + 1, bill5=self.bill5, bill10=self.bill10)
+      # print(opt_chg)
+      return
+    
+    elif (self.spare == 1):
+      print('Impossible de rendre la monnaie')
+      return None
 
+    else:
+      # 10 bills
+      if ( self.spare >= 10 ):
+        self.spare = self.TOTAL_AMOUNT % 10
+        self.bill10 = int((self.TOTAL_AMOUNT - self.spare) / 10)
+        self.get_the_change()
+      
+      # 5 bills
+      elif ( self.spare in [5, 7, 9]):
+        self.bill5 = 1
+        self.spare -= 5
+        self.get_the_change()
 
-def main():
+      # 2 coins
+      else:
+        self.coin2 = int((self.spare - (self.spare % 2)) / 2)
+        self.spare = self.spare % 2
+        self.get_the_change()
+    
+
+def optimal_change():
   change = Change()
+  change.ask_amount_to_change()
+  change.get_the_change()
+  print(change)
+  return change
 
 if __name__ == '__main__':
-  main()
+  optimal_change()
--- monnaie/rendu_de_monnaie_test.py
+++ monnaie/rendu_de_monnaie_test.py
@@ -5,7 +5,18 @@ class TestChange(unittest.TestCase):
   def test_init(self):
     # Test init function of the Change class
     change = Change()
-    self.assertIs(change.bill10, None)
-    self.assertIs(change.bill5, None)
-    self.assertIs(change.coin2, None)
+    self.assertIs(change.bill10, 0)
+    self.assertIs(change.bill5, 0)
+    self.assertIs(change.coin2, 0)
+
+  def test_amout_to_change(self):
+    # Test if input is of type int greater than 0
+    change = Change()
+    change.ask_amount_to_change()
     self.assertIsInstance(change.TOTAL_AMOUNT, int)
+    self.assertGreaterEqual(change.TOTAL_AMOUNT, 0)
+
+  def test_get_the_change(self):
+    # Test some change cases
+    # change = Change()
+    pass
